904cb3a9c3a9a75a7db7004c266564f12c8a460f
RBRibeiro00 <romuloribeirob@hotmail.com>
2023-09-12 16:26:07 -0300

    Jwt Service / Methods for create, extract and validate token

---
+++ stateless/stateless-auth-api/stateless-auth-api/src/main/java/br/com/rbribeiro/microservice/statelessauthapi/core/dto/AuthRequest.java
@@ -0,0 +1,4 @@
+package br.com.rbribeiro.microservice.statelessauthapi.core.dto;
+
+public record AuthRequest(String username, String password) {
+}
---
+++ stateless/stateless-auth-api/stateless-auth-api/src/main/java/br/com/rbribeiro/microservice/statelessauthapi/core/dto/TokenDTO.java
@@ -0,0 +1,4 @@
+package br.com.rbribeiro.microservice.statelessauthapi.core.dto;
+
+public record TokenDTO(String accessToken) {
+}
