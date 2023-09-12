05cdfb687ba9a335a47fc08868b8068c22a45d9a
RBRibeiro00 <romuloribeirob@hotmail.com>
2023-09-12 08:41:34 -0300

    Configuring projects

---
+++ stateful/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+«
+{stateful-any-api/stateful-any-api/src/main/java/br/com/rbribeiro/microservice/statefulanyapi/StatefulAnyApiApplication.java,3\f\3fe78e0ccab13c10124007016fe5c6053dbe1d46
+^
+.stateful-any-api/stateful-any-api/build.gradle,7\9\790c4fd3cf9144b84944914f9348fd0a5ffc4446
---
+++ stateless/stateless-auth-api/stateless-auth-api/src/main/java/br/com/rbribeiro/microservice/statelessauthapi/StatelessAuthApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rbribeiro.microservice.statelessauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatelessAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatelessAuthApiApplication.class, args);
+	}
+
+}
